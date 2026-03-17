ea5529a9dfb826f5140bf9b879e976537332d6f6
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-03-17 18:40:31 +0300

    feat(organizations): integrate TypeOrmModule for Organization entity
    - Added TypeOrmModule to organizations.module.ts to enable database interactions
    for the Organization entity.
    - This integration supports the management of organization data within the application.

--- src/modules/organizations/organizations.module.ts
+++ src/modules/organizations/organizations.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organization } from './organizations.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Organization])],
   controllers: [],
   providers: [],
 })
