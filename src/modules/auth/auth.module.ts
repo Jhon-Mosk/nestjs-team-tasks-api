7c5dead9c19807b7f1dd97f9fb418d55447fe7f8
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-03-18 20:02:24 +0300

    feat(auth): enhance AuthModule with JWT and Passport integration
    - Integrated PassportModule and JwtModule into AuthModule for improved authentication handling.
    - Configured TypeOrmModule to include User and Organization entities.
    - Added AuthController and AuthService to manage authentication processes.
    - Implemented JwtStrategy for JWT-based authentication.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organization } from '../organizations/organizations.entity';
+import { User } from '../users/users.entity';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    PassportModule,
+    JwtModule,
+    TypeOrmModule.forFeature([User, Organization]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
