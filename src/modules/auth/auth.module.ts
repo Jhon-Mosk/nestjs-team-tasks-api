e6cac8bb56279e08581654a1a322cc8a751e2126
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-03-20 20:28:10 +0300

    feat(auth): integrate RedisModule into AuthModule for enhanced session management
    - Added RedisModule to AuthModule to support session storage and management.
    - This integration aims to improve performance and scalability of authentication processes.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Organization } from '../organizations/organizations.entity';
+import { RedisModule } from '../redis/redis.module';
 import { User } from '../users/users.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -13,6 +14,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule,
     TypeOrmModule.forFeature([User, Organization]),
+    RedisModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
