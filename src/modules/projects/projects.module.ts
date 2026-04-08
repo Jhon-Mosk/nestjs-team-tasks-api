4813d8d026075dcc41c383a663c412a9e81c7362
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-04-08 15:13:34 +0300

    feat(projects): integrate ProjectsController and ProjectsService into ProjectsModule
    - Added ProjectsController to manage project-related requests.
    - Introduced ProjectsService to handle business logic for projects.
    - Updated ProjectsModule to include the new controller and
    service for improved project management functionality.

--- src/modules/projects/projects.module.ts
+++ src/modules/projects/projects.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsController } from './projects.controller';
 import { Project } from './projects.entity';
+import { ProjectsService } from './projects.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Project])],
-  controllers: [],
-  providers: [],
+  controllers: [ProjectsController],
+  providers: [ProjectsService],
 })
 export class ProjectsModule {}
