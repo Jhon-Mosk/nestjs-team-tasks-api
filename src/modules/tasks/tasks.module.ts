b648612f3bb94bb717b6454107a42eac22d0c75b
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-04-13 18:07:38 +0300

    feat(tasks): add RedisModule and TasksListCacheService to TasksModule
    - Integrated `RedisModule` into `TasksModule` for enhanced caching capabilities.
    - Registered `TasksListCacheService` as a
    provider to manage task list caching, improving performance and response times.

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '../projects/projects.entity';
+import { RedisModule } from '../redis/redis.module';
 import { User } from '../users/users.entity';
+import { TasksListCacheService } from './tasks-list-cache.service';
 import { TasksController } from './tasks.controller';
 import { Task } from './tasks.entity';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task, Project, User])],
+  imports: [TypeOrmModule.forFeature([Task, Project, User]), RedisModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksListCacheService],
 })
 export class TasksModule {}
