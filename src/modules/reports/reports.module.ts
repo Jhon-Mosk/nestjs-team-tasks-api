41810dc0404001a4bc61d98ef180f9d952f253c4
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-04-15 14:54:04 +0300

    feat(reports): enhance ReportsModule with UsersModule and BullMQ integration
    - Integrated UsersModule to enable user-related functionalities within the ReportsModule.
    - Registered a new BullMQ queue named 'reports-tasks' for managing report job processing.
    - Added ReportsController and ReportsService to handle report-related requests and business logic.
    - This update improves the modularity and functionality of the reporting system.

--- src/modules/reports/reports.module.ts
+++ src/modules/reports/reports.module.ts
@@ -1,8 +1,18 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
+import { ReportsService } from './report.service';
+import { ReportsController } from './reports.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    UsersModule,
+    BullModule.registerQueue({
+      name: 'reports-tasks',
+    }),
+  ],
+  controllers: [ReportsController],
+  providers: [ReportsService],
+  exports: [ReportsService],
 })
 export class ReportsModule {}
