ff8270271b6f55616369c0077609cc4cff1fa898
Evgeniy Politiko <jhon-mosk@yandex.ru>
2026-04-20 15:27:28 +0300

    feat(reports): enhance ReportsModule with TasksModule integration
    - Added TasksModule to the ReportsModule imports to facilitate
    task-related reporting functionalities.
    - Exported TasksService from TasksModule to enable its usage in other modules.
    - This update improves the modularity and functionality of the
    reporting system by integrating task management capabilities.

--- src/modules/reports/reports.module.ts
+++ src/modules/reports/reports.module.ts
@@ -1,16 +1,18 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { QUEUE_NAMES } from 'src/queue/queue.constants';
+import { EventsModule } from '../events/events.module';
+import { TasksModule } from '../tasks/tasks.module';
 import { UsersModule } from '../users/users.module';
 import { TasksReportProcessor } from './processors/tasks-report-processor';
 import { ReportsService } from './report.service';
-import { ReportsController } from './reports.controller';
-import { EventsModule } from '../events/events.module';
 import { ReportsEventsService } from './reports-events.service';
+import { ReportsController } from './reports.controller';
 
 @Module({
   imports: [
     UsersModule,
+    TasksModule,
     BullModule.registerQueue({
       name: QUEUE_NAMES.REPORTS_TASKS,
     }),
